ea1209f881df0e03f724709c10701198e5bc8ba8
howpwn <finn79426@gmail.com>
2018-09-07 15:35:50 +0800

    Add develop package

---
+++ 1_develop.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+# By : howpwn
+# Email : finn79426@gmail.com
+
+sudo apt-fast install 	cmake \
+						python-pip \
+						python3-pip \
+						python-dev \
+						build-essential \
+						virtualenvwrapper \
+						libffi-dev \
+						nodejs \
+						npm \
+						default-jre \
+						default-jdk
+
+pip install --upgrade virtualenv
+
